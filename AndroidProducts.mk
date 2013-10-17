7fd132e46b69f093bd373cc2de859d7cbc16b79a
xplodwild <xplod@ouverta.fr>
2013-10-17 15:18:54 +0100

    flo: custom -> omni
    Change-Id: I83d2bb9cf0e0467cfe81cbacebaa886aae309005

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,5 +17,5 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_flo.mk \
     $(LOCAL_DIR)/full_flo.mk \
-    $(LOCAL_DIR)/custom_flo.mk
+    $(LOCAL_DIR)/omni_flo.mk
 
--- omni_flo.mk
+++ omni_flo.mk
@@ -22,9 +22,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration 
-$(call inherit-product, vendor/custom/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_NAME := custom_flo
+PRODUCT_NAME := omni_flo
 PRODUCT_DEVICE := flo
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Nexus 7 2013
