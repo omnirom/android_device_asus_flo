8cd675581712e352739ea0f313c56bb924862861
havok87 <andre.sookram@gmail.com>
2014-03-27 19:09:55 -0400

    flo: use common_tablet.mk
    Change-Id: I8cdbfbe1454f36cbbab5a98c6bef71d13b34d627

--- omni_flo.mk
+++ omni_flo.mk
@@ -22,7 +22,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration 
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/common_tablet.mk)
 
 PRODUCT_NAME := omni_flo
 PRODUCT_DEVICE := flo
