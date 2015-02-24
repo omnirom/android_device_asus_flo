9e3005d9aa3a315556f8f979d3d45a099f2568e7
BeansTown106 <nbenis106@gmail.com>
2015-02-23 22:55:10 -0500

    flo: add missing media props
    Change-Id: Ie621c1cc6a30e79061752956235b9ee0dfc98f9c

--- device-common.mk
+++ device-common.mk
@@ -281,6 +281,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     fmas.spkr_angles=10 \
     fmas.spkr_sgain=0
 
+# missing props
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.media.effect.bgdropper.adj=0.2 \
+    camera.flash_off=0 \
+    ro.com.widevine.cachesize=16777216 
+
 PRODUCT_PACKAGES += \
     power.flo
 
