5d5dcd5f481a84e4e44a8b55738079e343e17ab9
Adhitya Mohan <me@adhityamohan.in>
2017-07-07 19:50:53 +0530

    flo:Use proper build fingerprint

--- omni_flo.mk
+++ omni_flo.mk
@@ -39,3 +39,8 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, device/asus/flo/device.mk)
 $(call inherit-product, vendor/asus/flo/flo-vendor.mk)
 #$(call inherit-product-if-exists, vendor/qcom/proprietary/common/config/device-vendor.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=razor \
+    BUILD_FINGERPRINT=google/razor/flo:6.0.1/MOB30X/3036618:user/release-keys \
+    PRIVATE_BUILD_DESC="razor-user 6.0.1 MOB30X 3036618 release-keys"
