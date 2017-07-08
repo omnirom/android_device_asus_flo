363d08e786890832e9530ba1a5822978af01e47a
Adhitya Mohan <me@adhityamohan.in>
2017-07-08 13:05:45 +0530

    Flo: use the correct defconfig

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,7 +29,7 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=flo user_
 BOARD_KERNEL_CMDLINE += vmalloc=340M
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 TARGET_KERNEL_SOURCE := kernel/asus/flo
-TARGET_KERNEL_CONFIG := lineageos_flo_defconfig
+TARGET_KERNEL_CONFIG := flo_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
