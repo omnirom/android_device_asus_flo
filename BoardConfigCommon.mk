158afcf8bf3d1d2110cfb577a30d2d78ca8ef30f
Adhitya Mohan <me@adhityamohan.in>
2017-07-10 04:35:21 +0000

    flo : Switch to the correct toolchain - arm-linux-androideabi- seems to cause a lot of linker errors for HALS -But instead of switching to an older GCC arm-linux-androidkernel- works perfectly

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -30,7 +30,7 @@ BOARD_KERNEL_CMDLINE += vmalloc=340M
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 TARGET_KERNEL_SOURCE := kernel/asus/flo
 TARGET_KERNEL_CONFIG := flo_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androidkernel-
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
