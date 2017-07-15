2a8b9347afe8da4cb6b6a25135b48c2ab646d6f2
Adhitya Mohan <me@adhityamohan.in>
2017-07-15 16:01:49 +0530

    Revert "flo: remove execmod from sepolicy"
    -Also go enforcing
    
    This reverts commit ba453c9acc47035f5bec4fc1efccb557fa96b0a3.
    
    Change-Id: I19e3ef7f7cf8da51d3c148f0f1e48871f8def5a2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -25,13 +25,12 @@ TARGET_NO_BOOTLOADER := false
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 # BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=msm8960 maxcpus=2
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=flo user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=flo user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += vmalloc=340M
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 TARGET_KERNEL_SOURCE := kernel/asus/flo
 TARGET_KERNEL_CONFIG := flo_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androidkernel-
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 BOARD_USES_ALSA_AUDIO:= true
